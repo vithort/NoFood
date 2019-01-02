325b961f79c704155b6f039c880cf05f61bac167
Vithor <vithort@gmail.com>
2019-01-02 20:35:27 -0300

    Definindo nossas Rotas (1)
    Se tiver algum erro de importação, executar o comando abaixo para atualizar as dependencias:
    > npm install
    ou
    > npm i
    
    Adicionando novas rotas (a GET já havia sido criada):
    - post
    - put
    - delete
    
    verificar no google para identificar cada código: status code http
    para testar os Verbs, usar o programa: postman

--- NoFood.Api/server.js
+++ NoFood.Api/server.js
@@ -2,11 +2,43 @@
 const express = require('express');
 const app = express();
 
+// CRUD
+
+// Verbs Http
+
+// (C)REATE
+// (R)EAD
+// (U)PDATE
+// (D)ELETE
+
 // definir uma rota:
+// (R)EAD
 app.get('/', (req, res) => {
-    // resposta de status 200 é quando obteve sucesso
+    // req = request
+    // res = response: resposta de status 200 é quando obteve sucesso
     res.status(200).send('Olá Mundo!');
 });
+
+// (C)REATE
+app.post('/', (req, res) => {
+    res.status(201).send('Criado!');
+});
+
+// (U)PDATE
+app.put('/', (req, res) => {
+    res.status(202).send('Atualizado!');
+});
+
+// (D)ELETE
+app.delete('', (req, res) => {
+    res.status(204).send('Registro Excluído!');
+});
+
+// 400 - Bad Request
+// 401 - Unauthorized
+// 500 - Internal Server Error
+
+
 
 // faz o servidor escutar na porta 3000:
 app.listen(3000, () => {
