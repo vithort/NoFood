36654f7cc55a2fad2ffdf55974afe0d3eeb1bfb4
vithort <vithort@gmail.com>
2019-01-03 14:40:47 -0300

    Conectando no MongoDB
    Pode baixar o MongoDB, usar o MongoDB Atlas ou o MLab
    
    Instalar o Mongoose para conectar o app
    > npm install mongoose --save
    
    Fazemos a conexao no express.js e criamos a variavel em variables.js

--- NoFood.Api/bin/configuration/variables.js
+++ NoFood.Api/bin/configuration/variables.js
@@ -1,6 +1,9 @@
 const variables = {
     Api: {
         port: process.env.port || 3000
+    },
+    Database: {
+        connection: process.env.connection || 'mongodb://admin:nofood123456@ds149034.mlab.com:49034/nofood'
     }
 };
 
--- NoFood.Api/bin/express.js
+++ NoFood.Api/bin/express.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const variables = require('../bin/configuration/variables');
 
 // Routers
 const categoriaRouter = require('../routes/categoria-router')
@@ -11,6 +13,10 @@ const app = express();
 // Configuração de parse do JSON
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+
+// Configurando a conexão com o Banco de Dados
+// Parâmetros de Configuração: mongodb://<dbuser>:<dbpassword>@ds149034.mlab.com:49034/nofood
+mongoose.connect(variables.Database.connection);
 
 // Configurando as rotas
 app.use('/api/categoria', categoriaRouter);
