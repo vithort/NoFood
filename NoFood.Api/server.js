196c6eef41cc6354f40c4d348f251c71546b3ca2
Vithor <vithort@gmail.com>
2019-01-02 21:07:51 -0300

    Definindo nossas Rotas (2)
    Desenvolvimento das rotas...
    
    Para não precisar ficar parando e iniciando o node com as alterações, é possível usar o nodemon (o -g é para global, em todas as aplicações):
    > npm install -g nodemon
    
    Para rodar o app:
    > nodemon server
    
    Para entender o Body, é preciso também instalar o body-parser:
    > npm install body-parser --save
    
    Precisa declarar o body-parser também:
    :: const bodyParser = require('body-parser');
    :: app.use(bodyParser.json());
    :: app.use(bodyParser.urlencoded({extended: false}));
    
    Testar no Postman!!!

--- NoFood.Api/server.js
+++ NoFood.Api/server.js
@@ -1,10 +1,16 @@
 //pega um pacote que já foi importado:
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 // CRUD
 
 // Verbs Http
+
+let pessoas = [];
 
 // (C)REATE
 // (R)EAD
@@ -16,22 +22,34 @@ const app = express();
 app.get('/', (req, res) => {
     // req = request
     // res = response: resposta de status 200 é quando obteve sucesso
-    res.status(200).send('Olá Mundo!');
+    res.status(200).send(pessoas);
 });
 
 // (C)REATE
 app.post('/', (req, res) => {
-    res.status(201).send('Criado!');
+    console.log("Body: ", req.body);
+    pessoas.push(req.body);
+    res.status(201).send(req.body);
 });
 
 // (U)PDATE
-app.put('/', (req, res) => {
-    res.status(202).send('Atualizado!');
+app.put('/:id', (req, res) => {
+    let pessoaEncontrada = pessoas.filter(pes => {
+        return pes.id == req.params.id;
+    })
+    pessoaEncontrada = req.body;
+    res.status(202).send(pessoaEncontrada);
 });
 
 // (D)ELETE
-app.delete('', (req, res) => {
-    res.status(204).send('Registro Excluído!');
+app.delete('/:id', (req, res) => {
+    for (let index = 0; index < pessoas.length; index++) {
+        const pessoa = pessoas[index];
+        if (pessoa.id == req.params.id) {
+            pessoas.splice(index, 1);
+        }
+    }
+    res.status(204).send();
 });
 
 // 400 - Bad Request
