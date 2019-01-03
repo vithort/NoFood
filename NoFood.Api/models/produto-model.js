ff9398b3aa2686087a0b19e7e138bc1459839c8c
vithort <vithort@gmail.com>
2019-01-03 15:44:57 -0300

    Criando o schema do documento
    Criar os esquemas: categoria-model.js e produto-model.js
    
    Ajustar o Controller

---
+++ NoFood.Api/models/produto-model.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+
+const produtoModel = new schema({
+    nome: {trim: true, index: true, required: true, type: String},
+    descricao: {type: String, required: true},
+    foto: {type: String, required: true},
+    preco: {type: Number, required: true},
+    ativa: {type: Boolean, required: true},
+    dataCriacao: {type: Date, default: Date.now}
+}, {versionKey: false});
+
+produtoModel.pre('save', next => {
+    let agora = new Date();
+    if (!this.dataCriacao ) {
+        this.dataCriacao = agora;
+    }
+    next();
+});
+
+module.exports = mongoose.model('Produto', produtoModel);
