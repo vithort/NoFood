b4db3768878d097f1600d4f625315faa5f18aec5
vithort <vithort@gmail.com>
2019-01-11 15:02:06 -0300

    Utilizando o Ionic Generate
    Extensões Snippets:
     - ESLint
     - TSLint
     - SCSS Formatter
     - Auto Import
     - ionic 2 Commands with Snippets
     - Ionic 2 snippets
     - Ionic 3 snippets
    
    Criar e alterar as cores no variables.scss
    
    Criar as páginas:
    > ionic g page login
    > ionic g page categoria
    > ionic g page tabs
    > ionic g page produtos
    > ionic g page meusPedidos
    > ionic g page minhaConta
    > ionic g page tabCategoria
    
    Removemos a página "home" que será substituída por "login"
    
    Em app.components.ts incluir a LoginPage como LazingLoad para ser a primeira páguina a ser iniciada

---
+++ NoFood.Ionic/src/pages/tab-categoria/tab-categoria.ts
@@ -0,0 +1,24 @@
+import { CategoriaPage } from './../categoria/categoria';
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, App } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-tab-categoria',
+  templateUrl: 'tab-categoria.html',
+})
+export class TabCategoriaPage {
+
+  constructor(
+    public navCtrl: NavController
+    , public navParams: NavParams
+    , private app: App
+  ) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad TabCategoriaPage');
+    this.app.getRootNav().setRoot(CategoriaPage);
+  }
+
+}
