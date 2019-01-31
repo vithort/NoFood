75c21d640bef0826ca48d2ba5c93bd3ffec9d1fd
vithort <vithort@gmail.com>
2019-01-31 14:34:47 -0300

    Criando a Listagem de Categorias (...)
    Ajustando as Categorias...

--- NoFood.Ionic/src/pages/categoria/categoria.ts
+++ NoFood.Ionic/src/pages/categoria/categoria.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ActionSheetController } from 'ionic-angular';
+import { CategoriaProvider } from './../../providers/categoria/categoria';
+import { CategoriaModel } from './../../app/models/categoria-model';
 
 @IonicPage()
 @Component({
@@ -8,14 +10,59 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CategoriaPage {
 
+  categorias: Array<CategoriaModel> = new Array<CategoriaModel>();
+
   constructor(
     public navCtrl: NavController,
-    public navParams: NavParams
+    public navParams: NavParams,
+    private categoriaSrv: CategoriaProvider,
+    public actionSheetController: ActionSheetController
   ) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CategoriaPage');
+  }
+
+  ionViewWillEnter() {
+    this.load();
+  }
+
+  async load(): Promise<void> {
+    try {
+      let categoriaResult = await this.categoriaSrv.get();
+      if (categoriaResult.success) {
+        this.categorias = <Array<CategoriaModel>>categoriaResult.data;
+      }
+    } catch (error) {
+      console.log('Problema ao carregar as Categorias! Erro: ', error);
+    }
+  }
+
+  adminOptions(): void {
+    let action = this.actionSheetController.create({
+      title: 'Administração',
+      buttons: [
+        {
+          text: 'Gerenciar Categorias',
+          handler: () => {
+            this.gerenciarCategoria();
+          }
+        },
+        {
+          text: 'Gerenciar Produtos',
+          handler: () => {
+            this.gerenciarProduto();
+          }
+        },
+        {
+          text: 'Cancelar',
+          handler: () => { },
+          role: 'destructive'
+        }
+      ]
+    });
+    action.present();
   }
 
   gerenciarCategoria(): void {
